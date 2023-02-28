7a80d0e9cbc870568a984346cb6555bb6a0dee65
sahilgupta <sahilgupta742@gmail.com>
2023-03-01 01:08:11 +0530

    Data Structures and Algorithms

--- src/LinkedList2/CircularDoublyLLUSe.java
+++ src/LinkedList2/CircularDoublyLLUSe.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CircularDoublyLLUSe {
 
     public static DoublyNode<Integer> takeInput() {
-        DoublyNode<Integer> head = null, tail = null, prev = null;
+        DoublyNode<Integer> head = null, tail = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the data: ");
          int data = sc.nextInt();
@@ -14,7 +14,8 @@ public class CircularDoublyLLUSe {
             if (head == null) {
                 head = newNode;
                 tail = newNode;
-                newNode.prev = newNode;
+                head.prev = newNode;
+                head.next = newNode;
 
             } else {
                 tail.next = newNode;
@@ -32,35 +33,37 @@ public class CircularDoublyLLUSe {
 
         if(pos == 0){
             newNode.next = head;
-            DoublyNode<Integer> lastNode = head.prev;//beacause of circular head ke previos mai lastNode ka adress hoga
+            DoublyNode<Integer> lastNode = head.prev;//because of circular head ke previous mai lastNode ka address hoga
             lastNode.next = newNode;
             newNode.prev = lastNode;
             head = newNode;
             head.next.prev = newNode;
             return head;
-        }
+        }else {
 
-        int i = 0; //intitially i is on the head so it is zero
-        DoublyNode<Integer> temp = head;// Temp mai jisko bola hai head pr rehne ko
-        while (i != pos - 1 ){
-            temp = temp.next;// aage badh te jao
-            i++;//i ko bhi aage badhao
+            int i = 0; //intitially i is on the head so it is zero
+            DoublyNode<Integer> temp = head;// Temp mai jisko bola hai head pr rehne ko
+            while (i != pos - 1) {
+                temp = temp.next;// aage badh te jao
+                i++;//i ko bhi aage badhao
+            }
+            //in LinkedList create links before breaking the old ones
+            newNode.next = temp.next;//newNode ke next mai that means the one we want to insert
+            //uske next mai aage waale ke next ka address store kar
+            newNode.prev = temp;//newNode ke prev mai that means the one we want to insert
+            temp.next = newNode;//uske baad jo temp ka next hai uske newNode ka address store karo
+            newNode.next.prev = newNode;
         }
-        //in LinkedList create links before breaking the old ones
-        newNode.next = temp.next;//newNode ke next mai that means the one we want to insert
-                               //uske next mai aage waale ke next ka address store kar
-        newNode.prev = temp;//newNode ke prev mai that means the one we want to insert
-        temp.next = newNode;//uske baad jo temp ka next hai uske newNode ka address store karo
-        newNode.next.prev = newNode;
         return head;
     }
 
 
 //    Print function will be same
     public static void print(DoublyNode<Integer> head){
-        while (head !=null){
-            System.out.print(head.data+ " ");
-            head = head.next;
+        DoublyNode<Integer> current = head;
+        while (current != head.prev){
+            System.out.print(current.data+ " ");
+            current = current.next;
         }
         System.out.println();
 
