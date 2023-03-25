d31770eecfc51173495e827a8cb46f44fcf83a1a
sahilgupta <sahilgupta742@gmail.com>
2023-03-26 01:12:03 +0530

    Data Structures and Algorithms

---
+++ src/Trees/GreaterThanX.java
@@ -0,0 +1,90 @@
+/*/*
+Given a tree and an integer x, find and return number of Nodes which are greater than x.
+Input format :
+Single Line : First Integer denotes x and rest of the elements in level order form separated by space. Order is -
+Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element
+Output Format :
+Count of nodes greater than x
+Sample Input 1 :
+35 10 3 20 30 40 2 40 50 0 0 0 0
+Sample Output 1 :
+3
+Sample Input 2 :
+10 10 3 20 30 40 2 40 50 0 0 0 0
+Sample Output 2:
+5
+*/
+
+package Trees;
+
+import java.util.Scanner;
+
+public class GreaterThanX {
+
+    //    Taking Input Level Wise using Queues
+    public static TreeNode<Integer> takeInputLevelWise() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the root data:");
+        int rootData = sc.nextInt();
+        QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        TreeNode<Integer> root = new TreeNode<>(rootData);
+        pendingNodes.enqueue(root);
+        while (!pendingNodes.isEmpty()) {
+            try {
+                TreeNode<Integer> frontNode = pendingNodes.dequeue();
+                System.out.println("Enter the number of child of " + frontNode.data);
+                int numChildren = sc.nextInt();
+                for (int i = 0; i < numChildren; i++) {
+                    System.out.println("Enter " + (i + 1) + "th child of" + frontNode.data);
+                    int child = sc.nextInt();
+                    TreeNode<Integer> childNode = new TreeNode<>(child);
+                    frontNode.children.add(childNode);//connecting the childNode and the frontNode
+                    pendingNodes.enqueue(childNode);//here we add the childNode because maybe it contains children further
+                }
+            } catch (QueueEmptyException e) {
+                //it will not come here
+            }
+        }
+        return root;
+    }
+    //    printing tree Level Wise using Queues
+    public static void printLevelWise(TreeNode<Integer> root) throws QueueEmptyException {
+        if (root == null) {
+            return;
+        }
+
+        QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<>();
+        pendingNodes.enqueue(root);
+        while (!pendingNodes.isEmpty()) {// until queue became empty
+            TreeNode<Integer> frontNode = pendingNodes.dequeue();// root ko nikala
+            System.out.print(frontNode.data + ": ");//root ko print karaya
+            for (int i = 0; i < frontNode.children.size(); i++) {//phir uske bacho tak traverse karaya
+                System.out.print(frontNode.children.get(i).data + ",");// phir bachhe print kara liye
+                pendingNodes.enqueue(frontNode.children.get(i));//phir baccho ko queue mai bhej dia
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int greaterThanX(TreeNode<Integer> root, int x){
+        if(root == null){//EDGE CASE
+            return 0;
+        }
+        int count = 0;
+        if (root.data > x) {//Base CASE
+            count++;
+        }
+
+        for (int i = 0; i <root.children.size() ; i++) {
+           count = count + greaterThanX(root.children.get(i), x);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws QueueEmptyException {
+        TreeNode<Integer> root = takeInputLevelWise();
+        printLevelWise(root);
+        System.out.println(greaterThanX(root, 10));
+    }
+}
