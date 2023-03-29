16ccb0ca382e23b0d82f1fde1a76c2a46bb5d286
sahilgupta <sahilgupta742@gmail.com>
2023-03-30 01:00:45 +0530

    commit

---
+++ src/BinaryTree/TraversalBT_PreOrder.java
@@ -0,0 +1,55 @@
+package BinaryTree;
+
+import java.util.Scanner;
+
+public class TraversalBT_PreOrder {
+    public static BinaryTreeNode<Integer> takeInputLevelWise(){
+        Scanner s = new Scanner(System.in);
+        QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes =new QueueUsingLL<>();
+        System.out.println("Enter root data:");
+        int rootData = s.nextInt();
+        if (rootData == -1) {
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        pendingNodes.enqueue(root);
+        while (!pendingNodes.isEmpty()){
+            BinaryTreeNode<Integer> front;
+            try {
+                front = pendingNodes.dequeue();
+            } catch (QueueEmptyException e) {
+                return null;
+            }
+            System.out.println("Enter the left child of"+ front.data);
+            int leftChild = s.nextInt();
+            if (leftChild != -1) {
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<>(leftChild);
+                pendingNodes.enqueue(child);
+                front.left = child;
+            }
+
+            System.out.println("Enter the right child of"+ front.data);
+            int rightChild = s.nextInt();
+            if (rightChild != -1) {
+                BinaryTreeNode<Integer> child = new BinaryTreeNode<>(rightChild);
+                pendingNodes.enqueue(child);
+                front.right = child;
+            }
+        }
+        return root;
+    }
+
+    public static void preOrderTraversal(BinaryTreeNode<Integer> root){
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data+ " ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = takeInputLevelWise();
+        preOrderTraversal(root);
+    }
+}
